6a7b5ac6e91fbaa855bf881b1e441561374d658b
jessegumtow <gumtow@gmail.com>
2020-08-01 15:40:35 -0500

    file uploader workign with Cloudinary API

--- foster_app_client/src/components/ChildEdit.js
+++ foster_app_client/src/components/ChildEdit.js
@@ -9,6 +9,8 @@ export default class ChildEdit extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            image: {},
+            file: {}
             
         };
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -32,18 +34,16 @@ export default class ChildEdit extends Component {
     }
 
     handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
+        const updateInput = Object.assign( this.state.child, {[event.target.name]: event.target.value})
+        this.setState(updateInput);
     };
 
     handleSubmit(event) {
         axios.put(`http://localhost:3001/children/${this.props.match.params.id}`, {
                 name: this.state.name,
-                status: this.state.status,
-                pictures: this.state.pictures,
-                documents: this.state.documents
+                status: this.state.status
         }, 
+        this.onFileUpload(),
         { withCredentials: true }
         ).then(response =>{
             this.props.history.push("/dashboard");
@@ -52,6 +52,33 @@ export default class ChildEdit extends Component {
         // getChildData();
         event.preventDefault();
     };
+
+        // On file select
+        onFileChange = event => {
+            event.persist()
+            this.setState(()=>{
+                return{
+                    [event.target.name]: event.target.files[0]
+                }
+            })
+        };
+    
+        // file upload button clicked
+        onFileUpload = () => {
+            const formData = new FormData();
+    
+            formData.append("image", this.state.image)
+            formData.append("child_id", this.state.child.id)
+            formData.append("file", this.state.file)
+            // console.log(this.state.selectedFile);
+    
+            axios.post("http://localhost:3001/items", formData, {withCredentials: true})
+            .then( response =>{
+                console.log(response);
+            }).catch(error => {
+                console.log("ITEM ERROR", error);
+            });
+        };
 
     handleLogoutClick=()=> {
         axios.delete("http://localhost:3001/logout", { withCredentials: true }).then(response =>{
@@ -69,10 +96,10 @@ export default class ChildEdit extends Component {
                     <button onClick={()=>this.handleLogoutClick()}>Logout</button>
                     <h1>Hello {this.state.child.name}</h1>
                     <form onSubmit={this.handleSubmit} >
-                        <input type="string" name="name" placeholder={this.state.child.name} value={this.state.name} onChange={this.handleChange} required />
-                        <input type="string" name="status" placeholder={this.state.child.status} value={this.state.status} onChange={this.handleChange} required />
-                        <input type="string" name="pictures" value={this.state.pictures} onChange={this.handleChange} required />
-                        <input type="string" name="documents" value={this.state.documents} onChange={this.handleChange} required />
+                        <input type="string" name="name" value={this.state.child.name} onChange={this.handleChange} required />
+                        <input type="string" name="status" value={this.state.child.status} onChange={this.handleChange} required />
+                        <input type="file" name="image" placeholder="Upload an image" onChange={this.onFileChange} required />
+                        <input type="file" name="file" placeholder="Upload a document" onChange={this.onFileChange} />
                         <button type="submit">Update Child</button>
                     </form>
                 </div>
